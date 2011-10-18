b3f0811e3b81aee527510299a454f1d14a31e08a
Paul Yoder <paulyoder@gmail.com>
2011-10-18 13:52:42 -0500

    added SymetricEncryption class

--- NetUtility/Program.cs
+++ NetUtility/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace NetUtility
@@ -9,6 +10,16 @@ namespace NetUtility
     {
         static void Main(string[] args)
         {
+            var value = "Hello World!";
+            var encryptor = new SymetricEncryption();
+            var iv = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            var encrypted = encryptor.Encrypt(value, "password", iv);
+            var decrypted = encryptor.Decrypt(encrypted, "password", iv);
+
+            Console.WriteLine("Encrypted: {0}", encrypted);
+            Console.WriteLine("Decrypted: {0}", decrypted);
+            Console.ReadLine();
         }
     }
 }
---
+++ NetUtility/SymetricEncryption.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace NetUtility
+{
+    public class SymetricEncryption
+    {
+        public string Encrypt(string value, string password, byte[] iv)
+        {
+            var aes = new RijndaelManaged();
+            var passwordDerived = new Rfc2898DeriveBytes(password, Encoding.ASCII.GetBytes("SaltyThePig"), 2);
+            aes.Key = passwordDerived.GetBytes(256 / 8);
+            aes.IV = iv;
+            aes.Mode = CipherMode.CBC;
+            var encryptor = aes.CreateEncryptor();
+            using (var mStream = new MemoryStream())
+            using (var cStream = new CryptoStream(mStream, encryptor, CryptoStreamMode.Write))
+            using (var sWriter = new StreamWriter(cStream))
+            {
+                sWriter.Write(value);
+                sWriter.Close();
+                return Convert.ToBase64String(mStream.ToArray());
+            }
+        }
+        
+        public string Decrypt(string cypher, string password, byte[] iv)
+        {
+            var aes = new RijndaelManaged();
+            var passwordDerived = new Rfc2898DeriveBytes(password, Encoding.ASCII.GetBytes("SaltyThePig"), 2);
+            aes.Key = passwordDerived.GetBytes(256 / 8);
+            aes.IV = iv;
+            aes.Mode = CipherMode.CBC;
+            var decryptor = aes.CreateDecryptor();
+            using (var mStream = new MemoryStream(Convert.FromBase64String(cypher)))
+            using (var cStream = new CryptoStream(mStream, decryptor, CryptoStreamMode.Read))
+            using (var sReader = new StreamReader(cStream))
+            {
+                return sReader.ReadToEnd();
+            }
+        }
+    }
+}
